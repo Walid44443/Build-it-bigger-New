c93e9c0003eea9a62325c2c69a409f1f9445327c
walid4444 <midoma4444@gmail.com>
2017-09-15 16:20:00 +0200

    Finish all project except testing

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,24 +1,18 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.test.espresso.ViewInteraction;
+
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Pair;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import walid44443.fb.androidlibrary.DisplayJokeActivity;
+import java.util.Arrays;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
+import walid44443.fb.mylib.Jokes;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by walid4444 on 9/15/17.
@@ -32,12 +26,15 @@ public class MainActivityTest {
     @Test
     public void MainActivityTestMethod(){
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withText("Tell Joke"),
-                        withParent(allOf(withId(R.id.fragment),
-                                withParent(withId(android.R.id.content)))),
-                        isDisplayed()));
+        new EndpointsAsyncTask() {
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                if (Arrays.asList(Jokes.joke_arr).contains(result) && !result.isEmpty() && result != null)
+                    assertNotNull(result);
 
-        //appCompatButton.perform(click());
+            }
+        };
+
     }
 }
